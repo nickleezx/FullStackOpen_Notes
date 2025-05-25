be40960ae455d8d3fd2ef05c90652935b3f980f6
nickleezx <nickleezx@gmail.com>
2025-05-25 16:37:52 +0800

    part 4 changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
 const notesRouter = require('./controllers/notes');
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter);
 app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,16 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')){
+    return authorization.replace('Bearer ', '')
+  }
+
+  return null
+}
 
 notesRouter.get('/', async (req, res) => {
   const notes = await Note
@@ -18,7 +28,12 @@ notesRouter.get('/:id', async (req, res, next) => {
 })
 
 notesRouter.post('/', async (req, res, next) => {
-  const user = await User.findById(req.body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+  if (!decodedToken.id)
+    return res.status(401).json({error: 'token invalid'})
+
+
+  const user = await User.findById(decodedToken.id)
   
   if(!user)
     return res.status(400).json({error: 'userId missing or not valid'})
